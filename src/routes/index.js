44937a7f66c817e3eeda3cdf8a807db9baa86793
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-11 09:43:28 -0300

    Configurando rotas

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Swith, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Swith>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Swith>
+  );
+}
