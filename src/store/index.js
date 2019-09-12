bd548ec8b6daf6141d09ade83a13eb3dbf0dfeab
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-12 19:11:45 -0300

    persistindo autenticação

--- src/store/index.js
+++ src/store/index.js
@@ -16,8 +16,9 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(rootReducer, middlewares);
+const store = createStore(persistReducers(rootReducer), middlewares);
+const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
 
-export default store;
+export { store, persistor };
