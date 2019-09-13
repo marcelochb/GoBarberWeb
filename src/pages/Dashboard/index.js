9cf1505dcb700fd275a1f69dc650179623e4ee97
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-13 18:08:29 -0300

    Requisição autenticada

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Dasshboard() {
+  api.get('providers');
   return <h1>Dashboard</h1>;
 }
