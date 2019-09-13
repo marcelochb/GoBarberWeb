f8247571f18c7ecfa113a6c9f83c2f6d893b1b8f
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-13 20:17:17 -0300

    Estilizando notificações

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
-import logo from '~/assets/logo-purple.svg';
-
 import { Link } from 'react-router-dom';
 
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
@@ -16,6 +16,7 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Marcelo Block</strong>
