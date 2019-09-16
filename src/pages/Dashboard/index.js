b06db48d9bf7e61f813a80a0b28b8759f31750a2
Marcelo Block Teixeira <marcelochb@gmail.com>
2019-09-16 00:03:31 -0300

    Estilização do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import api from '~/services/api';
+// import api from '~/services/api';
 
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dasshboard() {
   return (
@@ -17,6 +17,24 @@ export default function Dasshboard() {
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Diego Fernandes</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em Aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Marcelo Block</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Maria Luiza</span>
+        </Time>
+      </ul>
     </Container>
   );
 }
